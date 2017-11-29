9a9e5a2dd1020e715480868991227ef40aae82c2
Stuart Wallace <stuartw@atom.net>
2017-11-29 15:57:10 +0000

    Added (bad) wrapper around STL RNG functions in order to expose a random-unsigned-int generator; added Net::Util::getRandomHardwareAddress()

--- brewctl/util.cc
+++ brewctl/util.cc
@@ -7,11 +7,14 @@
 */
 
 #include "util.h"
+#include <chrono>
 #include <cstdlib>      // NULL
 #include <cstring>      // ::strcpy
+#include <random>
 
 extern "C"
 {
+#include <netdb.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
@@ -19,15 +22,56 @@ extern "C"
 #include <unistd.h>
 }
 
+using std::chrono::duration;
+using std::chrono::high_resolution_clock;
 using std::string;
 using std::vector;
 
 
 namespace Util
 {
+namespace Random
+{
+
+static unsigned int seedVal_ = 0;
+
+
+// seed() - seed the random-number generator with the specified <seedVal>.
+//
+void seed(unsigned int seedVal) noexcept
+{
+    seedVal_ = seedVal;
+}
+
+
+// seed() - seed the random-number generator with the current time from the high-resolution clock
+//
+void seed() noexcept
+{
+    seed(high_resolution_clock::now().time_since_epoch().count());
+}
+
+
+// randomUnsignedInt() - return a random unsigned integer.
+//
+unsigned int randomUnsignedInt() noexcept
+{
+    std::minstd_rand0 generator(seedVal_);
+    unsigned int ret = generator();
+
+    seedVal_ = ret;
+
+    return ret;
+}
+
+
+} // namespace Random
+
 namespace Net
 {
+
 const unsigned short HWADDR_LEN = 6;        // Length, in bytes, of a MAC address
+
 
 // getInterfaceNames() - return the names of all network interfaces in the system, in the form of a vector<string>.
 //
@@ -48,8 +92,7 @@ vector<string> getInterfaceNames() noexcept
     return ret;
 }
 
-
-// getInterfaceHardwareAddress() - get the hardware (MAC) address of the specified interface, as a hex string.
+// getInterfaceHardwareAddress() - get the hardware (MAC) address of the specified interface, as a uint64_t.
 //
 uint64_t getInterfaceHardwareAddress(const string& interface) noexcept
 {
@@ -57,21 +100,32 @@ uint64_t getInterfaceHardwareAddress(const string& interface) noexcept
     struct ifreq ifr;
     uint64_t ret = 0;
 
-    sock = ::socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock == -1)
+    sock = ::socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if(sock < 0)
         return (uint64_t) -1;
 
     ::strcpy(ifr.ifr_name, interface.c_str());
 
-    if(::ioctl(sock, SIOCGIFHWADDR, &ifr) == -1)
+    if(::ioctl(sock, SIOCGIFHWADDR, &ifr) != 0)
         return (uint64_t) -1;
 
-    for(i = 0; i < HWADDR_LEN; ++i, ret <<= 8)
-        ret += ((unsigned char *) ifr.ifr_hwaddr.sa_data)[i];
+    for(i = 0; i < HWADDR_LEN; ++i)
+        ret = (ret << 8) + (unsigned char) (ifr.ifr_hwaddr.sa_data)[i];
 
     ::close(sock);
 
     return ret;
+}
+
+
+// getRandomHardwareAddress() - get a random hardware (MAC) address as a uint64_t.
+//
+uint64_t getRandomHardwareAddress() noexcept
+{
+    uint64_t ret = Util::Random::randomUnsignedInt();
+    ret = (ret << 32) + Util::Random::randomUnsignedInt();
+
+    return ret & 0xffffffffffff;
 }
 
 } // namespace Net
--- brewctl/util.h
+++ brewctl/util.h
@@ -8,16 +8,27 @@
     Part of brewctl
 */
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 
 namespace Util
 {
+namespace Random
+{
+
+    void                        seed(unsigned int seedVal) noexcept;
+    void                        seed() noexcept;
+    unsigned int                randomUnsignedInt() noexcept;
+
+} // namespace Random
+
 namespace Net
 {
     std::vector<std::string>    getInterfaceNames() noexcept;
     uint64_t                    getInterfaceHardwareAddress(const std::string& interface) noexcept;
+    uint64_t                    getRandomHardwareAddress() noexcept;
 
 } // namespace Net
 } // namespace Util
